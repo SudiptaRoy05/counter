f433a4a5b98c05a98fa7ed82ae5b260a8708eada
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-07-05 12:47:25 +0600

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Navbar from './component/Navbar';
 
 function App() {
   const [value, setValue] = useState(0)
   return (
     <div className="App">
+      <Navbar/>
+
      <div className='value'>{value}</div>
      <button onClick={()=>{setValue(value+1)}}>Click Me</button>
     </div>
